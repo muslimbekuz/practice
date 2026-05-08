76a2f81d3a6878c7fd24d4519fe22e7160f1cdf8
muslimbekuz <muslimbekkhalimjonov@gmail.com>
2026-05-09 00:07:38 +0900

    fix: scope and priority

--- functions.py
+++ functions.py
@@ -44,3 +44,18 @@ print("result3:", result3)
 
 result4 = give_greet("Ali")
 print("result4:", result4)
+
+
+print("==== Scope ====")
+b = 100  # 3
+
+# DEFINE
+
+
+def calculate(a, b):  # 2
+    c = a*b  # 1
+    print(f"the c value: {c}")
+
+
+# CALL
+calculate(5, 50)
