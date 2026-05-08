bc0c6e3a162aca28aa2014144654f0c3ee3fa64f
muslimbekuz <muslimbekkhalimjonov@gmail.com>
2026-05-08 22:23:51 +0900

    feat: python system variables & special methods via dunders

--- run.py
+++ run.py
@@ -1,2 +1,14 @@
+# Dunder __builtins__, __init__
 message = "Hello world!"
 print(message)
+
+result = type(message)
+print("result:", result)
+
+''' in python, there are builtin tools:
+(1) TYPES > int float str list dict
+(2) FUNCTION > print() len() input() type()
+(#) CONSTANS > True False None
+'''
+
+print(dir(__builtins__))
