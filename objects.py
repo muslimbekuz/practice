ecfa0f1d26ea23803691b592c1c4ae4facdc8063
muslimbekuz <muslimbekkhalimjonov@gmail.com>
2026-05-12 15:06:14 +0900

    fix: handle all kind of errors

--- objects.py
+++ objects.py
@@ -29,13 +29,11 @@ car_dict = dict(name="Tayota", year=2026, electric=True)
 
 try:
     print("passed here!")
-    a = car_dict.speed
+    # a = car_dict.speed
     result = car_dict["origin"]
     print("result:", result)
-except KeyError as err:
-    print("No origin state property found:", err)
-except AttributeError as err:
-    print("No speed found:", err)
+except Exception as err:
+    print("General Error:", err)
 else:
     print("Executed successfully without errors")
 finally:
